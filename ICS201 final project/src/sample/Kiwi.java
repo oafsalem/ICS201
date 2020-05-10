b6ed683b26e2ebf31028030bd8d1adce5857d7c8
oafsalem <oafsalem2000@gmail.com>
2020-05-10 06:48:05 +0300

    hitbox fix

--- ICS201 final project/src/sample/Kiwi.java
+++ ICS201 final project/src/sample/Kiwi.java
@@ -17,6 +17,9 @@ public class Kiwi extends Fruit {
         circle.setFill(new ImagePattern(super.getImage()));
         circle.setCenterX(0);
         circle.setCenterY(Math.max(Main.windowHeight * Math.random(), Main.windowHeight/2.0));
+        circle.setStrokeType(StrokeType.OUTSIDE);
+        circle.setStroke(Color.TRANSPARENT);
+        circle.setStrokeWidth(50 - 25);
         super.setShape(circle);
     }
 }
